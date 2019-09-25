df026d9a948207bab97e08047fdfe283eeb00bdc
wangsiqi <1404400355@qq.com>
2019-09-25 17:08:08 +0800

    修改了gittest的user第一次

---
+++ IDEACodes/gittest/src/main/java/com/baizhi/user.java
@@ -0,0 +1,14 @@
+package com.baizhi;
+
+/**
+ * @program: code
+ * @description: 宋迎香
+ * @author: 爱喝汽水的王机智
+ * @create: 2019-09-25 17:04
+ **/
+public class user {
+
+
+    private String id;
+    private String name;
+}
